1a81b829a5883f7caad090b1ab33ec9b8cec8c23
Davide Lombardo <davide.lombardo.92@gmail.com>
2025-08-30 17:55:46 +0200

    feat: add rate limiting

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 import recipesRouter from './routes/recipes';
@@ -10,6 +11,7 @@ import configRouter from './routes/config';
 import menuRouter from './routes/menu';
 import logger from './utils/logger';
 import { errorHandler } from './middleware/errorHandling';
+import apiLimiter from './middleware/rateLimit';
 import { kindeUserSync } from './middleware/kindeUserSync';
 import { getDb } from 'services/dbHelpers';
 
@@ -22,38 +24,39 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(kindeUserSync);
 
-app.use('/api/recipes', recipesRouter);
-app.use('/api/config', configRouter);
-app.use('/api/menu', menuRouter);
+app.use('/api/recipes', apiLimiter as any, recipesRouter);
+app.use('/api/config', apiLimiter as any, configRouter);
+app.use('/api/menu', apiLimiter as any, menuRouter);
 
-app.get('/api/debug/users', async (req, res) => {
-  const { db } = await getDb();
-  const result = db.exec('SELECT * FROM users');
-  res.json(result[0]?.values || []);
-});
-
-
-app.post('/api/debug/assign-recipes', async (req, res) => {
-  const { user_id } = req.body;
-  const { db, dbPath } = await getDb();
-  db.run('UPDATE recipes SET user_id = ?', [user_id]);
-  // Save DB to disk
-  const fs = await import('fs');
-  const data = db.export();
-  fs.writeFileSync(dbPath, Buffer.from(data));
-  res.json({ success: true });
-});
+// app.get('/api/debug/users', async (req, res) => {
+//   const { db } = await getDb();
+//   const result = db.exec('SELECT * FROM users');
+//   res.json(result[0]?.values || []);
+// });
 
 
-app.get('/api/debug/recipes', async (req, res) => {
-  const { db } = await getDb();
-  const result = db.exec('SELECT * FROM recipes');
-  res.json(result[0]?.values || []);
-});
+// app.post('/api/debug/assign-recipes', async (req, res) => {
+//   const { user_id } = req.body;
+//   const { db, dbPath } = await getDb();
+//   db.run('UPDATE recipes SET user_id = ?', [user_id]);
+//   // Save DB to disk
+//   const fs = await import('fs');
+//   const data = db.export();
+//   fs.writeFileSync(dbPath, Buffer.from(data));
+//   res.json({ success: true });
+// });
+
+
+// app.get('/api/debug/recipes', async (req, res) => {
+//   const { db } = await getDb();
+//   const result = db.exec('SELECT * FROM recipes');
+//   res.json(result[0]?.values || []);
+// });
 
 // Error handler middleware
 app.use(errorHandler);
